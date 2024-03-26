505074148e7d31eca912226895930150577e335e
JackDotJS <jackdotjs@proton.me>
2024-03-25 21:11:20 -0700

    wip: add gallery page

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,7 @@ import './global.css';
 import App from './App';
 
 // Routes
+const Gallery = lazy(() => import(`./pages/Gallery`));
 const Specs = lazy(() => import(`./pages/Specs`));
 const Commissions = lazy(() => import(`./pages/Commissions`));
 const Links = lazy(() => import(`./pages/Links`));
@@ -14,6 +15,7 @@ const Links = lazy(() => import(`./pages/Links`));
 render(() => (
   <Router root={App}>
     <Route path="/" />
+    <Route path="/gallery" component={Gallery} />
     <Route path="/specs" component={Specs} />
     <Route path="/commissions" component={Commissions} />
     <Route path="/links" component={Links} />
---
+++ src/pages/Gallery.tsx
@@ -0,0 +1,17 @@
+ 
+import type { Component } from 'solid-js';
+
+import styles from './Gallery.module.css';
+
+// TODO: literally everything
+const Gallery: Component = () => {
+  return (
+    <main>
+      <h2>under construction</h2>
+
+      <p>i can't be bothered to rebuild this part of the site yet, come back later</p>
+    </main>
+  );
+};
+
+export default Gallery;
