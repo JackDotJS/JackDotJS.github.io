4bf18ef5734de0a8284c96984bf72c1fe7a0a522
JackDotJS <jackdotjs@proton.me>
2024-03-25 21:48:28 -0700

    footer: fix styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,22 +6,24 @@ import styles from './Footer.module.css';
 
 const Footer: Component = () => {
   return (
-    <footer>
+    <footer class={styles.footer}>
       <h2>i also made this website</h2>
-      <p>
+      <div>
         made with 
         <a href="https://www.solidjs.com/" rel="external">SolidJS</a>, 
         <a href="https://vitejs.dev/" rel="external">Vite</a>, and 
-        <a href="https://www.typescriptlang.org/" rel="external">TypeScript</a>.<br/>
-        <br/>
-        build date: <b><time id="gh-build" datetime="">[...]</time></b><br/>
-        <br/>
-        rev: <b><a id="gh-rev" href="">[...]</a></b>
-      </p>
-      <div class="button-list">
-        <a href="https://github.com/JackDotJS/JackDotJS.github.io" rel="external" title="Source Code">
-          <span>source code</span>
-        </a>
+        <a href="https://www.typescriptlang.org/" rel="external">TypeScript</a>.
+      </div>
+      <div class={styles.buildInfo}>
+        <div>
+          build date: <b><time id="gh-build" datetime="">[...]</time></b><br/>
+        </div>
+        <div>
+          rev: <b><a id="gh-rev" href="">[...]</a></b>
+        </div>
+      </div>
+      <div>
+        <a href="https://github.com/JackDotJS/JackDotJS.github.io" rel="external" title="Source Code">source code</a> (MIT License)
       </div>
     </footer>
   );
