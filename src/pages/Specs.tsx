4797c3997dc9dd42c92a148cbefde881222be011
JackDotJS <jackdotjs@proton.me>
2024-03-26 15:10:33 -0700

    specs: add os field

--- src/pages/Specs.tsx
+++ src/pages/Specs.tsx
@@ -10,6 +10,7 @@ const Specs: Component = () => {
       <h3>pc specs</h3>
       <a href="https://pcpartpicker.com/user/JackDotJS/saved/pGjqbv">full build on pcpartpicker.com</a>
       <ul class={styles.specList}>
+        <li><em>os:</em> Fedora Workstation 39 (KDE Plasma 5)</li>
         <li><em>cpu:</em> AMD Ryzen 7 7700X @ 4.5 GHz</li>
         <li><em>gpu:</em> NVIDIA GeForce RTX 4070 Ti @ 2310 MHz</li>
         <li><em>ram:</em> G.Skill Trident Z, 2x16GB DDR5 CL30 @ 6000 MHz</li>
