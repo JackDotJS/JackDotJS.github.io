48acf510f0fc2eaf1062252c6e9c7603eea0a60a
JackDotJS <jackdotjs@proton.me>
2024-03-26 14:30:29 -0700

    footer: add build date and revision hash logic

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,49 @@
-import type { Component } from 'solid-js';
+import { createSignal, type Component, onMount } from 'solid-js';
 
 import styles from './Footer.module.css';
 
-// TODO: add logic to load build date and revision id
-
 const Footer: Component = () => {
+  let buildDateElem!: HTMLTimeElement;
+  let revHashElem!: HTMLAnchorElement;
+
+  onMount(() => {
+    fetch(`gha-build.txt`).then(async (response) => {
+      if (response.status !== 200) {
+        return console.error(`couldn't fetch build date: ${response.status}`);
+      }
+
+      const text = await response.text();
+      const num = parseInt(text);
+
+      if (isNaN(num)) {
+        return console.error(`gha-build: ${num}`);
+      }
+
+      const ghbuild = new Date(num * 1000);
+
+      buildDateElem.textContent = ghbuild.toUTCString().toLocaleLowerCase(); // because uppercase letters are scary
+      buildDateElem.setAttribute(`datetime`, ghbuild.toISOString());
+    });
+
+    fetch(`gha-hash.txt`).then(async (response) => {
+      if (response.status !== 200) {
+        return console.error(`couldn't fetch revision hash: ${response.status}`);
+      }
+
+      const text = await response.text();
+      const ghrev = text.trim();
+
+      revHashElem.textContent = ghrev;
+      revHashElem.setAttribute(`href`, `https://github.com/JackDotJS/JackDotJS.github.io/commit/${ghrev}`);
+    });
+  });
+
   return (
     <footer class={styles.footer}>
       <h2>i also made this website</h2>
+      <div>
+        <a href="https://github.com/JackDotJS/JackDotJS.github.io" rel="external" title="Source Code">it's fully open-source</a>. (MIT License)
+      </div>
       <div>
         made with 
         <a href="https://www.solidjs.com/" rel="external">SolidJS</a>, 
@@ -16,14 +52,11 @@ const Footer: Component = () => {
       </div>
       <div class={styles.buildInfo}>
         <div>
-          build date: <b><time id="gh-build" datetime="">[...]</time></b><br/>
+          build date: <b><time ref={buildDateElem} datetime="">[...]</time></b><br/>
         </div>
         <div>
-          rev: <b><a id="gh-rev" href="">[...]</a></b>
+          rev: <b><a ref={revHashElem} href="">[...]</a></b>
         </div>
-      </div>
-      <div>
-        <a href="https://github.com/JackDotJS/JackDotJS.github.io" rel="external" title="Source Code">source code</a> (MIT License)
       </div>
     </footer>
   );
