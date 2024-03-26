be42b089b3735aeecd0460754109af2140e9b1c3
JackDotJS <jackdotjs@proton.me>
2024-03-25 20:34:04 -0700

    why root div when body do trick? #smort

--- src/index.tsx
+++ src/index.tsx
@@ -9,15 +9,9 @@ import App from './App';
 // Routes
 const Specs = lazy(() => import(`./pages/Specs`));
 
-const root = document.getElementById(`root`);
-
-if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
-  throw new Error(`Root element not found!`);
-}
-
 render(() => (
   <Router root={App}>
     <Route path="/" />
     <Route path="/specs" component={Specs} />
   </Router>
-), root!);
+), document.body);
