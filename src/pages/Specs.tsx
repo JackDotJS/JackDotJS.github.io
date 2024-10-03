81ff3ac3d725f2e7bb9ced046bfe66acc8336838
JackDotJS <jackdotjs@proton.me>
2024-10-03 02:35:06 -0700

    updated specs page

--- src/pages/Specs.tsx
+++ src/pages/Specs.tsx
@@ -10,19 +10,20 @@ const Specs: Component = () => {
       <h3>pc specs</h3>
       <a href="https://pcpartpicker.com/user/JackDotJS/saved/pGjqbv">full build on pcpartpicker.com</a>
       <ul class={styles.specList}>
-        <li><em>os:</em> Fedora Workstation 39 (KDE Plasma 5)</li>
-        <li><em>cpu:</em> AMD Ryzen 7 7700X @ 4.5 GHz</li>
-        <li><em>gpu:</em> NVIDIA GeForce RTX 4070 Ti @ 2310 MHz</li>
-        <li><em>ram:</em> G.Skill Trident Z, 2x16GB DDR5 CL30 @ 6000 MHz</li>
+        <li><b>os:</b> Fedora Linux 40 (KDE Plasma 6)</li>
+        <li><b>cpu:</b> AMD Ryzen 7 7700X @ 4.5 GHz</li>
+        <li><b>gpu:</b> NVIDIA GeForce RTX 4070 Ti @ 2310 MHz</li>
+        <li><b>ram:</b> G.Skill Trident Z, 2x16GB DDR5 CL30 @ 6000 MHz</li>
       </ul>
 
       <h3>peripherals</h3>
       <ul class={styles.specList}>
-        <li><em>display:</em> ViewSonic VP3481, 3440x1440 @ 100 Hz</li>
-        <li><em>keyboard:</em> Keychron K1 SE w/ Mint Optical Switches</li>
-        <li><em>mouse:</em> Logitech G305 @ 1000 DPI</li>
-        <li><em>microphone:</em> Razer Seiren Mini</li>
-        <li><em>headphones:</em> Bose QuietComfort 35 II</li>
+        <li><b>display:</b> ViewSonic VP3481, 3440x1440 @ 100 Hz</li>
+        <li><b>keyboard:</b> Keychron V3 w/ K Pro Mint Switches</li>
+        <li><b>mouse:</b> Logitech G305 @ 1000 DPI</li>
+        <li><b>controller:</b> PlayStation DualSense Wireless</li>
+        <li><b>microphone:</b> Razer Seiren Mini</li>
+        <li><b>headphones:</b> Bose QuietComfort 35 II</li>
       </ul>
 
       <h3>software</h3>
@@ -43,9 +44,9 @@ const Specs: Component = () => {
           <img src="/assets/software_icons/obs.svg" />
           OBS Studio
         </a>
-        <a href="https://www.blackmagicdesign.com/products/davinciresolve" rel="external">
-          <img src="/assets/software_icons/davinci.svg" />
-          DaVinci Resolve
+        <a href="https://kdenlive.org/" rel="external">
+          <img src="/assets/software_icons/kdenlive.svg" />
+          Kdenlive
         </a>
       </div>
     </main>
