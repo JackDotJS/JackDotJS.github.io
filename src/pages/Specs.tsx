87969375e9bc2362a2644c3fce12208d1c99f3c0
JackDotJS <jackdotjs@proton.me>
2025-05-30 14:32:45 -0700

    specs: adjust overall page width, misc minor changes
    Signed-off-by: JackDotJS <jackdotjs@proton.me>

--- src/pages/Specs.tsx
+++ src/pages/Specs.tsx
@@ -4,29 +4,27 @@ import styles from './Specs.module.css';
 
 const Specs: Component = () => {
   return (
-    <main class={styles.specs}>
-      <h2>things i use</h2>
-    
-      <h3>pc specs</h3>
-      <a href="https://pcpartpicker.com/user/JackDotJS/saved/pGjqbv">full build on pcpartpicker.com</a>
+    <main>
+      <h2>pc specs</h2>
       <ul class={styles.specList}>
         <li><em>os:</em> Fedora Linux 42 (KDE Plasma 6, Wayland)</li>
         <li><em>cpu:</em> AMD Ryzen 7 7700X</li>
         <li><em>gpu:</em> Sapphire PULSE Radeon RX 7900 XTX</li>
         <li><em>ram:</em> G.Skill Trident Z, 2x16GB DDR5 CL30</li>
       </ul>
+      <a href="https://pcpartpicker.com/user/JackDotJS/saved/pGjqbv">full build on pcpartpicker.com</a>
 
-      <h3>peripherals</h3>
+      <h2>peripherals</h2>
       <ul class={styles.specList}>
         <li><em>display:</em> ViewSonic VP3481, 3440x1440 @ 100 Hz</li>
         <li><em>keyboard:</em> Keychron V3 w/ K Pro Mint Switches</li>
         <li><em>mouse:</em> MCHOSE G3 Ultra @ 1000 DPI</li>
-        <li><em>controller:</em> PlayStation DualSense Wireless</li>
+        <li><em>gamepad:</em> PlayStation DualSense Wireless</li>
         <li><em>microphone:</em> Razer Seiren Mini</li>
         <li><em>headphones:</em> Bose QuietComfort 35 II</li>
       </ul>
 
-      <h3>software</h3>
+      <h2>software</h2>
       <div class={styles.icongrid}>
         <a href="https://www.blender.org/" rel="external">
           <div>
